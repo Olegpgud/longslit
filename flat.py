ebbcb6b33c23998e348b7190226039bf755a491b
polina <polina.pahomova96@mail.ru>
2017-07-19 15:49:09 +0300

    change with name

--- flat.py
+++ flat.py
@@ -6,11 +6,13 @@ from astropy.io import fits
 import astropy.io.fits.hdu.hdulist
 import os
 
-print('number slit:')
-number = int(input())
-print('with normalization - 1, without normalization - 0:')
-norm = int(input()) 
-hdus = fits.open('flat_slit'+repr(number)+'_jos_g-1.fits',memmap=True)
+f = open('../20170710/flat/flat7_H_list.txt')
+a=[]
+for line in f:
+    a.append(line.rstrip())
+print a
+norm = 1
+hdus = fits.open('../20170710/flat/'+a[1],memmap=True)
 master_flat = np.zeros((hdus[0].data.shape[0],hdus[0].data.shape[1]),dtype=np.float)
 n = hdus[0].header['NAXIS1']
 centr = hdus[0].data[n/4:3*n/4,n/4:3*n/4]
@@ -18,11 +20,8 @@ flux = np.sum(centr,0)
 flux_centr = np.sum(flux,0)
 hdus.close()
 
-name = 'flat_slit'+repr(number)+'_jos_g-'
-print('using fits:')
-for i in range(5):
-	hdu = fits.open(name+repr(i+1)+'.fits',memmap=True)
-	print(name+repr(i+1)+'.fits')
+for fl in a:
+	hdu = fits.open('../20170710/flat/'+fl,memmap=True)
 	flat = hdu[0].data
 	centr_flat = hdu[0].data[n/4:3*n/4,n/4:3*n/4]
 	flux_flat = np.sum(centr_flat,0)
@@ -34,8 +33,8 @@ for i in range(5):
 	master_flat = master_flat+flat*k
 	hdu.close()
 print('results:')
-stat = np.std(master_flat[100:200,100:200]/5)
-mean = np.mean(master_flat[100:200,100:200]/5)
+stat = np.std(master_flat[100:200,100:200]/len(a))
+mean = np.mean(master_flat[100:200,100:200]/len(a))
 print('stat = '+repr(stat)+'	mean = '+ repr(mean))
 
 try:
@@ -44,6 +43,6 @@ except OSError:
 	pass
 
 hdu = fits.PrimaryHDU()
-hdu.data = master_flat/5
-hdu.writeto('done_res/master_flat-'+repr(number)+'.fits', clobber=True)
+hdu.data = master_flat/len(a)
+hdu.writeto('done_res/master_flat.fits', clobber=True)
 
